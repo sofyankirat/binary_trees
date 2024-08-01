f869912b454e677f52c2a573a7b29c02de890e32
sofyankirat <www.sofyankirat123@gmail.com>
2024-08-01 07:32:45 -0700

    ds

--- 133-heap_extract.c
+++ 133-heap_extract.c
@@ -1,53 +1,108 @@
+#include <stdlib.h>
 #include "binary_trees.h"
 
-/**
- * tree_size - measures the sum of heights of a binary tree
- * @tree: pointer to the root node of the tree to measure the height
- *
- * Return: Height or 0 if tree is NULL
- */
-size_t tree_size(const binary_tree_t *tree)
-{
-	size_t height_l = 0;
-	size_t height_r = 0;
+size_t get_node_count(const heap_t *tree);
+heap_t *get_last_node(heap_t *root, size_t index, size_t size);
+void top_down_heapify(heap_t *root);
 
-	if (!tree)
+/**
+ * heap_extract - Extracts the root node of a Max Binary Heap
+ *
+ * @root: Double pointer to the root node of the heap
+ *
+ * Return: Value stored in the root node, or 0 on failure
+ */
+int heap_extract(heap_t **root)
+{
+	int value;
+	size_t size;
+	heap_t *last = NULL;
+
+	if (!root || !*root)
 		return (0);
 
-	if (tree->left)
-		height_l = 1 + tree_size(tree->left);
+	value = (*root)->n;
+	size = get_node_count(*root);
+	last = get_last_node(*root, 0, size);
 
-	if (tree->right)
-		height_r = 1 + tree_size(tree->right);
+	(*root)->n = last->n;
 
-	return (height_l + height_r);
+	if (last->parent)
+	{
+		if (last->parent->left == last)
+			last->parent->left = NULL;
+		else
+			last->parent->right = NULL;
+	}
+	else
+		*root = NULL;
+	free(last);
+	top_down_heapify(*root);
+	return (value);
 }
 
 /**
- * heap_to_sorted_array - converts a Binary Max Heap
- * to a sorted array of integers
- *
- * @heap: pointer to the root node of the heap to convert
- * @size: address to store the size of the array
- *
- * Return: pointer to array sorted in descending order
- **/
-int *heap_to_sorted_array(heap_t *heap, size_t *size)
+ * get_node_count - Counts the total number of nodes in a binary tree
+ * @tree: Pointer to the root node of the tree to count the number of nodes
+ * Return: Number of nodes in the tree
+ */
+size_t get_node_count(const heap_t *tree)
 {
-	int i, *a = NULL;
+	if (!tree)
+		return (0);
+	return (1 + get_node_count(tree->left) + get_node_count(tree->right));
+}
 
-	if (!heap || !size)
+/**
+ * get_last_node - Gets the last node of a Max Binary Heap
+ *
+ * @root: Pointer to the root node of the Heap
+ * @index: Index of the current node in the binary representation of the size
+ * @size: Size of the Heap
+ *
+ * Return: Pointer to the last node of the Heap
+ */
+heap_t *get_last_node(heap_t *root, size_t index, size_t size)
+{
+	heap_t *node = root;
+
+	if (!root || index >= size)
 		return (NULL);
+	if (index == size - 1)
+		return (node);
+	node = get_last_node(root->left, 2 * index + 1, size);
+	if (node)
+		return (node);
+	return (get_last_node(root->right, 2 * index + 2, size));
+}
 
-	*size = tree_size(heap) + 1;
+/**
+ * top_down_heapify - Moves the node at the top of the tree down to its
+ * correct position in the Max Binary Heap
+ *
+ * Description: This function compares the value of the current node with the
+ * values of its left and right children. If the value of the current node is
+ * less than the value of either of its children, the values of the current
+ * node and the child node with the largest value are swapped. This process
+ * is repeated until the current node is greater than both of its children.
+ *
+ * @root: Pointer to the root node of the Heap
+ * Return: Nothing
+ */
+void top_down_heapify(heap_t *root)
+{
+	heap_t *largest = root, *current = NULL;
+	int temp;
 
-	a = malloc(sizeof(int) * (*size));
-
-	if (!a)
-		return (NULL);
-
-	for (i = 0; heap; i++)
-		a[i] = heap_extract(&heap);
-
-	return (a);
+	while (largest != current)
+	{
+		current = largest;
+		if (current->left && current->left->n > current->n)
+			largest = current->left;
+		if (current->right && current->right->n > largest->n)
+			largest = current->right;
+		temp = current->n;
+		current->n = largest->n;
+		largest->n = temp;
+	}
 }
--- binary_trees.h
+++ binary_trees.h
@@ -1,12 +1,6 @@
-#ifndef _BINARY_TREES_H_
-#define _BINARY_TREES_H_
-
+#ifndef BINARY_TREES_H
+#define BINARY_TREES_H
 #include <stddef.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <limits.h>
-
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -22,26 +16,34 @@ struct binary_tree_s
 	struct binary_tree_s *left;
 	struct binary_tree_s *right;
 };
+
 typedef struct binary_tree_s binary_tree_t;
+
+/* Binary Search Tree */
 typedef struct binary_tree_s bst_t;
+
+/* AVL Tree */
 typedef struct binary_tree_s avl_t;
+
+/* Max Binary Heap */
 typedef struct binary_tree_s heap_t;
-/*  linked list for advanced 101 the levelorder */
+
 /**
- * struct link_s - structure for advanced tasks
+ * struct queue_node - Binary tree node
+ * Description: This structure is used to store a node in a queue
  *
- * @n: depth of node specified
- * @node: node of tree to store
- * @next: next node of the linked list
+ * @b_tree_node: Pointer to the binary tree node
+ * @next: Pointer to the next node
  */
-typedef struct link_s
+typedef struct queue_node
 {
-	size_t n;
-	struct binary_tree_s const *node;
-	struct link_s *next;
-} link_t;
-/*  ----------------- mandatory task ---------------------------------*/
-void binary_tree_print(const binary_tree_t *);
+	binary_tree_t *b_tree_node;
+	struct queue_node *next;
+} q_node;
+
+/* Print function */
+void binary_tree_print(const binary_tree_t *tree);
+
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
@@ -61,15 +63,17 @@ int binary_tree_is_full(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
-/*  ------------------ advanced task -----------------------------------*/
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
-const binary_tree_t *second);
+									 const binary_tree_t *second);
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
 int binary_tree_is_complete(const binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
 int binary_tree_is_bst(const binary_tree_t *tree);
 bst_t *bst_insert(bst_t **tree, int value);
+/* Helper functions - Moved here because of Betty */
+void create_queue(binary_tree_t *tree, q_node **head, q_node **tail,
+				  q_node *(*func)(binary_tree_t *, q_node **, q_node **));
 bst_t *array_to_bst(int *array, size_t size);
 bst_t *bst_search(const bst_t *tree, int value);
 bst_t *bst_remove(bst_t *root, int value);
@@ -84,4 +88,4 @@ heap_t *array_to_heap(int *array, size_t size);
 int heap_extract(heap_t **root);
 int *heap_to_sorted_array(heap_t *heap, size_t *size);
 
-#endif /* _BINARY_TREES_H_ */
+#endif /* BINARY_TREES_H */
